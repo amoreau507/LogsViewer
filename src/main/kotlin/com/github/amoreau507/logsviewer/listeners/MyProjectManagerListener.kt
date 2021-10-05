b604024ac253e4306bb100cc2564019af9b1bde2
GitHub Action <action@github.com>
2021-10-05 00:15:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/amoreau507/logsviewer/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/amoreau507/logsviewer/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.amoreau507.logsviewer.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.amoreau507.logsviewer.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/amoreau507/logsviewer/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/amoreau507/logsviewer/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.amoreau507.logsviewer.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.amoreau507.logsviewer.MyBundle
 
 class MyApplicationService {
 
